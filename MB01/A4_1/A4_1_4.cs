6ce90540552c7ceb4278758df7780c9a420ce994
Larissa Fitze <laerryies@gmail.com>
2023-10-31 15:55:55 +0100

    MB01

--- MB01/A4_1/A4_1_4.cs
+++ MB01/A4_1/A4_1_4.cs
@@ -19,6 +19,36 @@ namespace MB01.A8_2
 
         private void CmdCheck_Click(object sender, EventArgs e)
         {
+            int num1 = Convert.ToInt32(TxtValue1.Text);
+            int num2 = Convert.ToInt32(TxtValue2.Text);
+
+            for(int i =0; i < 5; i++)
+            {
+                LblResult.Text += i.ToString() + ";";
+            }
+
+
+
+            if (num1 > 0 && num2 > 0)
+            {
+                if (num1 == num2)
+                {
+                    LblResult.Text = "Ist gleich gross";
+                }
+                else if (num1 > num2)
+                {
+                    LblResult.Text = "A ist grösser";
+                }
+                else
+                {
+                    LblResult.Text = "B ist grösser";
+                }
+            }
+            else
+            {
+                MessageBox.Show("Die Zahl muss grösser 0 sein");
+            }
+
 
         }
     }
