7d1f90609d03fa53898d02640c2fd21a42aea6bf
Larissa Fitze <laerryies@gmail.com>
2023-11-07 14:30:11 +0100

    Aufgabe 8_2_1

--- MB01/A8_2/A8_2_1.Designer.cs
+++ MB01/A8_2/A8_2_1.Designer.cs
@@ -28,172 +28,186 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.LblValue1 = new System.Windows.Forms.Label();
-            this.LblValue2 = new System.Windows.Forms.Label();
-            this.TxtValue1 = new System.Windows.Forms.TextBox();
-            this.TxtValue2 = new System.Windows.Forms.TextBox();
-            this.GrpMath = new System.Windows.Forms.GroupBox();
-            this.RadbDiv = new System.Windows.Forms.RadioButton();
-            this.RadbMul = new System.Windows.Forms.RadioButton();
-            this.RadbSub = new System.Windows.Forms.RadioButton();
-            this.RadbAdd = new System.Windows.Forms.RadioButton();
-            this.LblResult = new System.Windows.Forms.Label();
-            this.TxtResult = new System.Windows.Forms.TextBox();
-            this.CmdCalculate = new System.Windows.Forms.Button();
-            this.GrpMath.SuspendLayout();
-            this.SuspendLayout();
+            LblValue1=new Label();
+            LblValue2=new Label();
+            TxtValue1=new TextBox();
+            TxtValue2=new TextBox();
+            GrpMath=new GroupBox();
+            RadbDiv=new RadioButton();
+            RadbMul=new RadioButton();
+            RadbSub=new RadioButton();
+            RadbAdd=new RadioButton();
+            LblResult=new Label();
+            TxtResult=new TextBox();
+            CmdCalculate=new Button();
+            GrpMath.SuspendLayout();
+            SuspendLayout();
             // 
             // LblValue1
             // 
-            this.LblValue1.AutoSize = true;
-            this.LblValue1.Location = new System.Drawing.Point(13, 16);
-            this.LblValue1.Name = "LblValue1";
-            this.LblValue1.Size = new System.Drawing.Size(43, 13);
-            this.LblValue1.TabIndex = 0;
-            this.LblValue1.Text = "1. Zahl:";
+            LblValue1.AutoSize=true;
+            LblValue1.Location=new Point(22, 31);
+            LblValue1.Margin=new Padding(5, 0, 5, 0);
+            LblValue1.Name="LblValue1";
+            LblValue1.Size=new Size(68, 25);
+            LblValue1.TabIndex=0;
+            LblValue1.Text="1. Zahl:";
             // 
             // LblValue2
             // 
-            this.LblValue2.AutoSize = true;
-            this.LblValue2.Location = new System.Drawing.Point(13, 45);
-            this.LblValue2.Name = "LblValue2";
-            this.LblValue2.Size = new System.Drawing.Size(43, 13);
-            this.LblValue2.TabIndex = 1;
-            this.LblValue2.Text = "2. Zahl:";
+            LblValue2.AutoSize=true;
+            LblValue2.Location=new Point(22, 87);
+            LblValue2.Margin=new Padding(5, 0, 5, 0);
+            LblValue2.Name="LblValue2";
+            LblValue2.Size=new Size(68, 25);
+            LblValue2.TabIndex=1;
+            LblValue2.Text="2. Zahl:";
             // 
             // TxtValue1
             // 
-            this.TxtValue1.Location = new System.Drawing.Point(63, 13);
-            this.TxtValue1.Name = "TxtValue1";
-            this.TxtValue1.Size = new System.Drawing.Size(162, 20);
-            this.TxtValue1.TabIndex = 2;
+            TxtValue1.Location=new Point(105, 25);
+            TxtValue1.Margin=new Padding(5, 6, 5, 6);
+            TxtValue1.Name="TxtValue1";
+            TxtValue1.Size=new Size(267, 31);
+            TxtValue1.TabIndex=2;
             // 
             // TxtValue2
             // 
-            this.TxtValue2.Location = new System.Drawing.Point(63, 39);
-            this.TxtValue2.Name = "TxtValue2";
-            this.TxtValue2.Size = new System.Drawing.Size(162, 20);
-            this.TxtValue2.TabIndex = 3;
+            TxtValue2.Location=new Point(105, 75);
+            TxtValue2.Margin=new Padding(5, 6, 5, 6);
+            TxtValue2.Name="TxtValue2";
+            TxtValue2.Size=new Size(267, 31);
+            TxtValue2.TabIndex=3;
+            TxtValue2.TextChanged+=TxtValue2_TextChanged;
             // 
             // GrpMath
             // 
-            this.GrpMath.Controls.Add(this.RadbDiv);
-            this.GrpMath.Controls.Add(this.RadbMul);
-            this.GrpMath.Controls.Add(this.RadbSub);
-            this.GrpMath.Controls.Add(this.RadbAdd);
-            this.GrpMath.Location = new System.Drawing.Point(16, 88);
-            this.GrpMath.Name = "GrpMath";
-            this.GrpMath.Size = new System.Drawing.Size(209, 67);
-            this.GrpMath.TabIndex = 4;
-            this.GrpMath.TabStop = false;
-            this.GrpMath.Text = "Mathematische Operationen";
+            GrpMath.Controls.Add(RadbDiv);
+            GrpMath.Controls.Add(RadbMul);
+            GrpMath.Controls.Add(RadbSub);
+            GrpMath.Controls.Add(RadbAdd);
+            GrpMath.Location=new Point(27, 169);
+            GrpMath.Margin=new Padding(5, 6, 5, 6);
+            GrpMath.Name="GrpMath";
+            GrpMath.Padding=new Padding(5, 6, 5, 6);
+            GrpMath.Size=new Size(348, 129);
+            GrpMath.TabIndex=4;
+            GrpMath.TabStop=false;
+            GrpMath.Text="Mathematische Operationen";
             // 
             // RadbDiv
             // 
-            this.RadbDiv.AutoSize = true;
-            this.RadbDiv.Location = new System.Drawing.Point(173, 31);
-            this.RadbDiv.Name = "RadbDiv";
-            this.RadbDiv.Size = new System.Drawing.Size(30, 17);
-            this.RadbDiv.TabIndex = 3;
-            this.RadbDiv.TabStop = true;
-            this.RadbDiv.Text = "/";
-            this.RadbDiv.UseVisualStyleBackColor = true;
+            RadbDiv.AutoSize=true;
+            RadbDiv.Location=new Point(288, 60);
+            RadbDiv.Margin=new Padding(5, 6, 5, 6);
+            RadbDiv.Name="RadbDiv";
+            RadbDiv.Size=new Size(44, 29);
+            RadbDiv.TabIndex=3;
+            RadbDiv.TabStop=true;
+            RadbDiv.Text="/";
+            RadbDiv.UseVisualStyleBackColor=true;
             // 
             // RadbMul
             // 
-            this.RadbMul.AutoSize = true;
-            this.RadbMul.Location = new System.Drawing.Point(118, 31);
-            this.RadbMul.Name = "RadbMul";
-            this.RadbMul.Size = new System.Drawing.Size(29, 17);
-            this.RadbMul.TabIndex = 2;
-            this.RadbMul.TabStop = true;
-            this.RadbMul.Text = "*";
-            this.RadbMul.UseVisualStyleBackColor = true;
+            RadbMul.AutoSize=true;
+            RadbMul.Location=new Point(197, 60);
+            RadbMul.Margin=new Padding(5, 6, 5, 6);
+            RadbMul.Name="RadbMul";
+            RadbMul.Size=new Size(45, 29);
+            RadbMul.TabIndex=2;
+            RadbMul.TabStop=true;
+            RadbMul.Text="*";
+            RadbMul.UseVisualStyleBackColor=true;
             // 
             // RadbSub
             // 
-            this.RadbSub.AutoSize = true;
-            this.RadbSub.Location = new System.Drawing.Point(64, 31);
-            this.RadbSub.Name = "RadbSub";
-            this.RadbSub.Size = new System.Drawing.Size(28, 17);
-            this.RadbSub.TabIndex = 1;
-            this.RadbSub.TabStop = true;
-            this.RadbSub.Text = "-";
-            this.RadbSub.UseVisualStyleBackColor = true;
+            RadbSub.AutoSize=true;
+            RadbSub.Location=new Point(107, 60);
+            RadbSub.Margin=new Padding(5, 6, 5, 6);
+            RadbSub.Name="RadbSub";
+            RadbSub.Size=new Size(44, 29);
+            RadbSub.TabIndex=1;
+            RadbSub.TabStop=true;
+            RadbSub.Text="-";
+            RadbSub.UseVisualStyleBackColor=true;
             // 
             // RadbAdd
             // 
-            this.RadbAdd.AutoSize = true;
-            this.RadbAdd.Location = new System.Drawing.Point(7, 31);
-            this.RadbAdd.Name = "RadbAdd";
-            this.RadbAdd.Size = new System.Drawing.Size(31, 17);
-            this.RadbAdd.TabIndex = 0;
-            this.RadbAdd.TabStop = true;
-            this.RadbAdd.Text = "+";
-            this.RadbAdd.UseVisualStyleBackColor = true;
+            RadbAdd.AutoSize=true;
+            RadbAdd.Location=new Point(12, 60);
+            RadbAdd.Margin=new Padding(5, 6, 5, 6);
+            RadbAdd.Name="RadbAdd";
+            RadbAdd.Size=new Size(49, 29);
+            RadbAdd.TabIndex=0;
+            RadbAdd.TabStop=true;
+            RadbAdd.Text="+";
+            RadbAdd.UseVisualStyleBackColor=true;
             // 
             // LblResult
             // 
-            this.LblResult.AutoSize = true;
-            this.LblResult.Location = new System.Drawing.Point(13, 169);
-            this.LblResult.Name = "LblResult";
-            this.LblResult.Size = new System.Drawing.Size(49, 13);
-            this.LblResult.TabIndex = 5;
-            this.LblResult.Text = "Resultat:";
+            LblResult.AutoSize=true;
+            LblResult.Location=new Point(22, 325);
+            LblResult.Margin=new Padding(5, 0, 5, 0);
+            LblResult.Name="LblResult";
+            LblResult.Size=new Size(78, 25);
+            LblResult.TabIndex=5;
+            LblResult.Text="Resultat:";
             // 
             // TxtResult
             // 
-            this.TxtResult.Location = new System.Drawing.Point(69, 166);
-            this.TxtResult.Name = "TxtResult";
-            this.TxtResult.Size = new System.Drawing.Size(156, 20);
-            this.TxtResult.TabIndex = 6;
+            TxtResult.Location=new Point(115, 319);
+            TxtResult.Margin=new Padding(5, 6, 5, 6);
+            TxtResult.Name="TxtResult";
+            TxtResult.Size=new Size(257, 31);
+            TxtResult.TabIndex=6;
             // 
             // CmdCalculate
             // 
-            this.CmdCalculate.Location = new System.Drawing.Point(16, 202);
-            this.CmdCalculate.Name = "CmdCalculate";
-            this.CmdCalculate.Size = new System.Drawing.Size(209, 23);
-            this.CmdCalculate.TabIndex = 7;
-            this.CmdCalculate.Text = "Berechne";
-            this.CmdCalculate.UseVisualStyleBackColor = true;
-            this.CmdCalculate.Click += new System.EventHandler(this.CmdCalculate_Click);
+            CmdCalculate.Location=new Point(27, 388);
+            CmdCalculate.Margin=new Padding(5, 6, 5, 6);
+            CmdCalculate.Name="CmdCalculate";
+            CmdCalculate.Size=new Size(348, 44);
+            CmdCalculate.TabIndex=7;
+            CmdCalculate.Text="Berechne";
+            CmdCalculate.UseVisualStyleBackColor=true;
+            CmdCalculate.Click+=CmdCalculate_Click;
             // 
-            // Form1
+            // A8_2_1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(237, 264);
-            this.Controls.Add(this.CmdCalculate);
-            this.Controls.Add(this.TxtResult);
-            this.Controls.Add(this.LblResult);
-            this.Controls.Add(this.GrpMath);
-            this.Controls.Add(this.TxtValue2);
-            this.Controls.Add(this.TxtValue1);
-            this.Controls.Add(this.LblValue2);
-            this.Controls.Add(this.LblValue1);
-            this.Name = "Form1";
-            this.Text = "Aufgabe_A8-2-1";
-            this.GrpMath.ResumeLayout(false);
-            this.GrpMath.PerformLayout();
-            this.ResumeLayout(false);
-            this.PerformLayout();
-
+            AutoScaleDimensions=new SizeF(10F, 25F);
+            AutoScaleMode=AutoScaleMode.Font;
+            ClientSize=new Size(395, 508);
+            Controls.Add(CmdCalculate);
+            Controls.Add(TxtResult);
+            Controls.Add(LblResult);
+            Controls.Add(GrpMath);
+            Controls.Add(TxtValue2);
+            Controls.Add(TxtValue1);
+            Controls.Add(LblValue2);
+            Controls.Add(LblValue1);
+            Margin=new Padding(5, 6, 5, 6);
+            Name="A8_2_1";
+            Text="Aufgabe_A8-2-1";
+            GrpMath.ResumeLayout(false);
+            GrpMath.PerformLayout();
+            ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
 
-        private System.Windows.Forms.Label LblValue1;
-        private System.Windows.Forms.Label LblValue2;
-        private System.Windows.Forms.TextBox TxtValue1;
-        private System.Windows.Forms.TextBox TxtValue2;
-        private System.Windows.Forms.GroupBox GrpMath;
-        private System.Windows.Forms.RadioButton RadbDiv;
-        private System.Windows.Forms.RadioButton RadbMul;
-        private System.Windows.Forms.RadioButton RadbSub;
-        private System.Windows.Forms.RadioButton RadbAdd;
-        private System.Windows.Forms.Label LblResult;
-        private System.Windows.Forms.TextBox TxtResult;
-        private System.Windows.Forms.Button CmdCalculate;
+        private Label LblValue1;
+        private Label LblValue2;
+        private TextBox TxtValue1;
+        private TextBox TxtValue2;
+        private GroupBox GrpMath;
+        private RadioButton RadbDiv;
+        private RadioButton RadbMul;
+        private RadioButton RadbSub;
+        private RadioButton RadbAdd;
+        private Label LblResult;
+        private TextBox TxtResult;
+        private Button CmdCalculate;
     }
 }
 
--- MB01/A8_2/A8_2_1.cs
+++ MB01/A8_2/A8_2_1.cs
@@ -13,7 +13,25 @@ namespace MB01.A8_2
 
         private void CmdCalculate_Click(object sender, EventArgs e)
         {
-  
+            sbyte value1 = Convert.ToSByte(TxtValue1.Text);
+            sbyte value2 = Convert.ToSByte(TxtValue2.Text);
+            int result = 0;
+
+            if (RadbAdd.Checked)
+            {
+                result = value1 + value2;
+            }
+            else if(RadbDiv.Checked)
+            {
+                result = (int)value1 / value2;
+            }
+
+            TxtResult.Text = result.ToString();
+        }
+
+        private void TxtValue2_TextChanged(object sender, EventArgs e)
+        {
+
         }
     }
 }
