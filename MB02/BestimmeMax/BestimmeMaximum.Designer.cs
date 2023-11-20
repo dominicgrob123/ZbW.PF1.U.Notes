e159374522fe586071c1d3adf0d7001b9b61e723
Larissa Fitze <laerryies@gmail.com>
2023-11-20 10:55:31 +0100

    ..

--- MB02/BestimmeMax/BestimmeMaximum.Designer.cs
+++ MB02/BestimmeMax/BestimmeMaximum.Designer.cs
@@ -28,164 +28,202 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.label1 = new System.Windows.Forms.Label();
-            this.label2 = new System.Windows.Forms.Label();
-            this.label3 = new System.Windows.Forms.Label();
-            this.TxtValue1 = new System.Windows.Forms.TextBox();
-            this.TxtValue2 = new System.Windows.Forms.TextBox();
-            this.label4 = new System.Windows.Forms.Label();
-            this.LblMax = new System.Windows.Forms.Label();
-            this.CmdMax1 = new System.Windows.Forms.Button();
-            this.TxtValue3 = new System.Windows.Forms.TextBox();
-            this.label5 = new System.Windows.Forms.Label();
-            this.CmdMax2 = new System.Windows.Forms.Button();
-            this.CmdMax3 = new System.Windows.Forms.Button();
-            this.SuspendLayout();
+            label1=new Label();
+            label2=new Label();
+            label3=new Label();
+            TxtValue1=new TextBox();
+            TxtValue2=new TextBox();
+            label4=new Label();
+            LblMax=new Label();
+            CmdMax1=new Button();
+            TxtValue3=new TextBox();
+            label5=new Label();
+            CmdMax2=new Button();
+            CmdMax3=new Button();
+            BtnArray=new Button();
+            button1=new Button();
+            SuspendLayout();
             // 
             // label1
             // 
-            this.label1.AutoSize = true;
-            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.Location = new System.Drawing.Point(13, 13);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(230, 29);
-            this.label1.TabIndex = 0;
-            this.label1.Text = "Bestimme Maximum";
+            label1.AutoSize=true;
+            label1.Font=new Font("Microsoft Sans Serif", 18F, FontStyle.Regular, GraphicsUnit.Point);
+            label1.Location=new Point(22, 25);
+            label1.Margin=new Padding(5, 0, 5, 0);
+            label1.Name="label1";
+            label1.Size=new Size(343, 40);
+            label1.TabIndex=0;
+            label1.Text="Bestimme Maximum";
             // 
             // label2
             // 
-            this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(18, 59);
-            this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(58, 13);
-            this.label2.TabIndex = 1;
-            this.label2.Text = "Erste Zahl:";
+            label2.AutoSize=true;
+            label2.Location=new Point(30, 113);
+            label2.Margin=new Padding(5, 0, 5, 0);
+            label2.Name="label2";
+            label2.Size=new Size(92, 25);
+            label2.TabIndex=1;
+            label2.Text="Erste Zahl:";
             // 
             // label3
             // 
-            this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(18, 89);
-            this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(66, 13);
-            this.label3.TabIndex = 2;
-            this.label3.Text = "Zweite Zahl:";
+            label3.AutoSize=true;
+            label3.Location=new Point(30, 171);
+            label3.Margin=new Padding(5, 0, 5, 0);
+            label3.Name="label3";
+            label3.Size=new Size(105, 25);
+            label3.TabIndex=2;
+            label3.Text="Zweite Zahl:";
             // 
             // TxtValue1
             // 
-            this.TxtValue1.Location = new System.Drawing.Point(102, 56);
-            this.TxtValue1.Name = "TxtValue1";
-            this.TxtValue1.Size = new System.Drawing.Size(128, 20);
-            this.TxtValue1.TabIndex = 3;
+            TxtValue1.Location=new Point(170, 108);
+            TxtValue1.Margin=new Padding(5, 6, 5, 6);
+            TxtValue1.Name="TxtValue1";
+            TxtValue1.Size=new Size(211, 31);
+            TxtValue1.TabIndex=3;
             // 
             // TxtValue2
             // 
-            this.TxtValue2.Location = new System.Drawing.Point(102, 87);
-            this.TxtValue2.Name = "TxtValue2";
-            this.TxtValue2.Size = new System.Drawing.Size(128, 20);
-            this.TxtValue2.TabIndex = 4;
+            TxtValue2.Location=new Point(170, 167);
+            TxtValue2.Margin=new Padding(5, 6, 5, 6);
+            TxtValue2.Name="TxtValue2";
+            TxtValue2.Size=new Size(211, 31);
+            TxtValue2.TabIndex=4;
             // 
             // label4
             // 
-            this.label4.AutoSize = true;
-            this.label4.Location = new System.Drawing.Point(18, 155);
-            this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(54, 13);
-            this.label4.TabIndex = 5;
-            this.label4.Text = "Maximum:";
+            label4.AutoSize=true;
+            label4.Location=new Point(30, 298);
+            label4.Margin=new Padding(5, 0, 5, 0);
+            label4.Name="label4";
+            label4.Size=new Size(95, 25);
+            label4.TabIndex=5;
+            label4.Text="Maximum:";
             // 
             // LblMax
             // 
-            this.LblMax.AutoSize = true;
-            this.LblMax.Location = new System.Drawing.Point(99, 155);
-            this.LblMax.Name = "LblMax";
-            this.LblMax.Size = new System.Drawing.Size(16, 13);
-            this.LblMax.TabIndex = 6;
-            this.LblMax.Text = ",,,";
+            LblMax.AutoSize=true;
+            LblMax.Location=new Point(165, 298);
+            LblMax.Margin=new Padding(5, 0, 5, 0);
+            LblMax.Name="LblMax";
+            LblMax.Size=new Size(24, 25);
+            LblMax.TabIndex=6;
+            LblMax.Text=",,,";
             // 
             // CmdMax1
             // 
-            this.CmdMax1.Location = new System.Drawing.Point(18, 180);
-            this.CmdMax1.Name = "CmdMax1";
-            this.CmdMax1.Size = new System.Drawing.Size(212, 23);
-            this.CmdMax1.TabIndex = 7;
-            this.CmdMax1.Text = "Maximum 1. und 2. Zahl";
-            this.CmdMax1.UseVisualStyleBackColor = true;
-            this.CmdMax1.Click += new System.EventHandler(this.CmdMax1_Click);
+            CmdMax1.Location=new Point(30, 346);
+            CmdMax1.Margin=new Padding(5, 6, 5, 6);
+            CmdMax1.Name="CmdMax1";
+            CmdMax1.Size=new Size(353, 44);
+            CmdMax1.TabIndex=7;
+            CmdMax1.Text="Maximum 1. und 2. Zahl";
+            CmdMax1.UseVisualStyleBackColor=true;
+            CmdMax1.Click+=CmdMax1_Click;
             // 
             // TxtValue3
             // 
-            this.TxtValue3.Location = new System.Drawing.Point(102, 118);
-            this.TxtValue3.Name = "TxtValue3";
-            this.TxtValue3.Size = new System.Drawing.Size(128, 20);
-            this.TxtValue3.TabIndex = 9;
+            TxtValue3.Location=new Point(170, 227);
+            TxtValue3.Margin=new Padding(5, 6, 5, 6);
+            TxtValue3.Name="TxtValue3";
+            TxtValue3.Size=new Size(211, 31);
+            TxtValue3.TabIndex=9;
             // 
             // label5
             // 
-            this.label5.AutoSize = true;
-            this.label5.Location = new System.Drawing.Point(18, 119);
-            this.label5.Name = "label5";
-            this.label5.Size = new System.Drawing.Size(59, 13);
-            this.label5.TabIndex = 8;
-            this.label5.Text = "Dritte Zahl:";
+            label5.AutoSize=true;
+            label5.Location=new Point(30, 229);
+            label5.Margin=new Padding(5, 0, 5, 0);
+            label5.Name="label5";
+            label5.Size=new Size(98, 25);
+            label5.TabIndex=8;
+            label5.Text="Dritte Zahl:";
             // 
             // CmdMax2
             // 
-            this.CmdMax2.Location = new System.Drawing.Point(18, 209);
-            this.CmdMax2.Name = "CmdMax2";
-            this.CmdMax2.Size = new System.Drawing.Size(212, 23);
-            this.CmdMax2.TabIndex = 10;
-            this.CmdMax2.Text = "Maximum 2. und 3. Zahl";
-            this.CmdMax2.UseVisualStyleBackColor = true;
-            this.CmdMax2.Click += new System.EventHandler(this.CmdMax2_Click);
+            CmdMax2.Location=new Point(30, 402);
+            CmdMax2.Margin=new Padding(5, 6, 5, 6);
+            CmdMax2.Name="CmdMax2";
+            CmdMax2.Size=new Size(353, 44);
+            CmdMax2.TabIndex=10;
+            CmdMax2.Text="Maximum 2. und 3. Zahl";
+            CmdMax2.UseVisualStyleBackColor=true;
+            CmdMax2.Click+=CmdMax2_Click;
             // 
             // CmdMax3
             // 
-            this.CmdMax3.Location = new System.Drawing.Point(18, 240);
-            this.CmdMax3.Name = "CmdMax3";
-            this.CmdMax3.Size = new System.Drawing.Size(212, 23);
-            this.CmdMax3.TabIndex = 11;
-            this.CmdMax3.Text = "Maximum 1. und 3. Zahl";
-            this.CmdMax3.UseVisualStyleBackColor = true;
-            this.CmdMax3.Click += new System.EventHandler(this.CmdMax3_Click);
+            CmdMax3.Location=new Point(30, 462);
+            CmdMax3.Margin=new Padding(5, 6, 5, 6);
+            CmdMax3.Name="CmdMax3";
+            CmdMax3.Size=new Size(353, 44);
+            CmdMax3.TabIndex=11;
+            CmdMax3.Text="Maximum 1. und 3. Zahl";
+            CmdMax3.UseVisualStyleBackColor=true;
+            CmdMax3.Click+=CmdMax3_Click;
             // 
-            // Form1
+            // BtnArray
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(254, 275);
-            this.Controls.Add(this.CmdMax3);
-            this.Controls.Add(this.CmdMax2);
-            this.Controls.Add(this.TxtValue3);
-            this.Controls.Add(this.label5);
-            this.Controls.Add(this.CmdMax1);
-            this.Controls.Add(this.LblMax);
-            this.Controls.Add(this.label4);
-            this.Controls.Add(this.TxtValue2);
-            this.Controls.Add(this.TxtValue1);
-            this.Controls.Add(this.label3);
-            this.Controls.Add(this.label2);
-            this.Controls.Add(this.label1);
-            this.Name = "Form1";
-            this.Text = "Aufgabe_10-1-1";
-            this.ResumeLayout(false);
-            this.PerformLayout();
-
+            BtnArray.Location=new Point(419, 346);
+            BtnArray.Name="BtnArray";
+            BtnArray.Size=new Size(230, 44);
+            BtnArray.TabIndex=12;
+            BtnArray.Text="Array erstellen";
+            BtnArray.UseVisualStyleBackColor=true;
+            BtnArray.Click+=BtnArray_Click;
+            // 
+            // button1
+            // 
+            button1.Location=new Point(426, 427);
+            button1.Name="button1";
+            button1.Size=new Size(223, 34);
+            button1.TabIndex=13;
+            button1.Text="Dictionary erstellen";
+            button1.UseVisualStyleBackColor=true;
+            button1.Click+=button1_Click;
+            // 
+            // BestimmeMaximum
+            // 
+            AutoScaleDimensions=new SizeF(10F, 25F);
+            AutoScaleMode=AutoScaleMode.Font;
+            ClientSize=new Size(828, 539);
+            Controls.Add(button1);
+            Controls.Add(BtnArray);
+            Controls.Add(CmdMax3);
+            Controls.Add(CmdMax2);
+            Controls.Add(TxtValue3);
+            Controls.Add(label5);
+            Controls.Add(CmdMax1);
+            Controls.Add(LblMax);
+            Controls.Add(label4);
+            Controls.Add(TxtValue2);
+            Controls.Add(TxtValue1);
+            Controls.Add(label3);
+            Controls.Add(label2);
+            Controls.Add(label1);
+            Margin=new Padding(5, 6, 5, 6);
+            Name="BestimmeMaximum";
+            Text="Aufgabe_10-1-1";
+            ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
 
-        private System.Windows.Forms.Label label1;
-        private System.Windows.Forms.Label label2;
-        private System.Windows.Forms.Label label3;
-        private System.Windows.Forms.TextBox TxtValue1;
-        private System.Windows.Forms.TextBox TxtValue2;
-        private System.Windows.Forms.Label label4;
-        private System.Windows.Forms.Label LblMax;
-        private System.Windows.Forms.Button CmdMax1;
-        private System.Windows.Forms.TextBox TxtValue3;
-        private System.Windows.Forms.Label label5;
-        private System.Windows.Forms.Button CmdMax2;
-        private System.Windows.Forms.Button CmdMax3;
+        private Label label1;
+        private Label label2;
+        private Label label3;
+        private TextBox TxtValue1;
+        private TextBox TxtValue2;
+        private Label label4;
+        private Label LblMax;
+        private Button CmdMax1;
+        private TextBox TxtValue3;
+        private Label label5;
+        private Button CmdMax2;
+        private Button CmdMax3;
+        private Button BtnArray;
+        private Button button1;
     }
 }
 
--- MB02/BestimmeMax/BestimmeMaximum.cs
+++ MB02/BestimmeMax/BestimmeMaximum.cs
@@ -12,19 +12,100 @@ namespace MB02.BestimmeMax
             InitializeComponent();
         }
 
+        public int GetMax(int num1, int num2)
+        {
+            var result = 0;
+            if (num1 > num2)
+            {
+                result = num1;
+            }
+            else
+            {
+                result = num2;
+            }
+
+            return result;
+        }
+
         private void CmdMax1_Click(object sender, EventArgs e)
         {
+            var num1 = Convert.ToInt32(TxtValue1.Text);
+            var num2 = Convert.ToInt32(TxtValue2.Text);
+            var maxValue = GetMax(num1, num2);
 
+            LblMax.Text = maxValue.ToString();
         }
+
 
         private void CmdMax2_Click(object sender, EventArgs e)
         {
+            var num1 = Convert.ToInt32(TxtValue3.Text);
+            var num2 = Convert.ToInt32(TxtValue2.Text);
+            var result = 0;
 
+            if (num1 > num2)
+            {
+                result = num1;
+            }
+            else
+            {
+                result = num2;
+            }
+
+            LblMax.Text = result.ToString();
         }
 
         private void CmdMax3_Click(object sender, EventArgs e)
         {
-   
+            var num1 = Convert.ToInt32(TxtValue3.Text);
+            var num2 = Convert.ToInt32(TxtValue1.Text);
+            var result = 0;
+
+            if (num1 > num2)
+            {
+                result = num1;
+            }
+            else
+            {
+                result = num2;
+            }
+
+            LblMax.Text = result.ToString();
+        }
+
+        private void BtnArray_Click(object sender, EventArgs e)
+        {
+            var arr = new int[3];
+            arr[0] = Convert.ToInt32(TxtValue1.Text);
+            arr[1] = Convert.ToInt32(TxtValue2.Text);
+            arr[2] = Convert.ToInt32(TxtValue3.Text);
+
+            var result = 0;
+
+            for (int i = 0; i < arr.Length; i++)
+            {
+                result += arr[i];
+            }
+
+
+
+
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            var dix = new Dictionary<string, double>();
+            var zahl = Convert.ToInt32(TxtValue1.Text);
+
+            dix.Add("Franz", zahl);
+            dix.Add("Fritz", zahl);
+
+            foreach (var element in dix) {
+
+                var a = element.Key + element.Value;
+            }
+
         }
     }
 }
