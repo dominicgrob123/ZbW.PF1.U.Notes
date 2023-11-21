f96321b2e25d935ff176eeaae0c16fdf8f55681d
Larissa Fitze <laerryies@gmail.com>
2023-11-21 16:10:45 +0100

    MB03

--- MB03/Adressverwaltung/AdressverwaltungView.cs
+++ MB03/Adressverwaltung/AdressverwaltungView.cs
@@ -12,6 +12,30 @@ namespace MB03.Adressverwaltung
 
         private void CmdCreatePerson_Click(object sender, EventArgs e)
         {
+            //Namespace Demo
+            var p = new MB03.Person();
+
+            //Variante 1
+            for (int i = 0; i < 1; i++)
+            {
+               Person person = new Person();
+               person.Age = -1;
+               TxtOutputArea.Text += person.Ausgabe();
+
+            }
+
+            //Zusatz
+            Person l = new Person("Larissa", 31);
+            TxtOutputArea.Text += l.Ausgabe();
+
+            Person c = new Person();
+            c.Name = "Cyrill";
+            c.Age = 35;
+            TxtOutputArea.Text += c.Ausgabe() ;
+
+
+            //Zusatz 2
+            var persons = new List<Person>() { new Person() { Name ="Mara", Age =25 } };
 
         }
 
--- MB03/Adressverwaltung/Person.cs
+++ MB03/Adressverwaltung/Person.cs
@@ -1,12 +1,76 @@
-﻿using System;
+﻿using static System.Windows.Forms.VisualStyles.VisualStyleElement;
+using System.Xml.Linq;
 
 namespace MB03.Adressverwaltung
 {
-    enum Color
+
+    public class Person
     {
-        blond, brown, red, black
-    }
-    class Person
-    {
+        //Klassenveriabeln
+        private string _name;
+        private int _age;
+
+        //Konstruktor
+        public Person(string name, int age) {
+            Name = name;
+            Age =age;
+        }
+
+        public Person(string name)
+        {
+            Name = name;
+            Age =12;
+        }
+
+        //Konstruktor
+        public Person() : this ("nix", -33)
+        {
+           
+        }
+
+        //Methode mit Rückgabewert
+        public string Ausgabe()
+        {
+            return Name + ": " + Age + "\r\n";
+        }
+
+        public string Ausgabe(string v)
+        {
+            return v + ": " + Age + "\r\n";
+        }
+
+        //Property
+        public string Name
+        {
+            get {
+                
+                return _name; }
+            set
+            {
+                if (string.IsNullOrEmpty(value))
+                {
+                    _name= "undefiniert";
+                }
+                else
+                {
+                    _name = value;
+                }
+
+                //andere Schreibweise eines if
+                //_name = string.IsNullOrEmpty(value) ? "undefiniert" : value;
+            }
+        }
+
+        public int Age
+        {
+            get { return _age; }
+            set
+            {
+                if (value >= 0)
+                {
+                    _age = value;
+                }
+            }
+        }
     }
 }
